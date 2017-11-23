7085a8a3e64c784d8f7062f0506e467a426e40af
JiaYing <s97531246@gmail.com>
2017-11-24 04:19:04 +0800

    Replace stack by using more registers in iterative

--- tests/fib_iterative.s
+++ tests/fib_iterative.s
@@ -2,46 +2,26 @@
 	.global	fib_iterative
 	.type	fib_iterative, @function
 fib_iterative:
-	dec	$sp, 16
-	sto.l	12($fp), $r0
-	ldi.l	$r0, -1
-	sto.l	-4($fp), $r0
-	ldi.l	$r0, 1
-	sto.l	-8($fp), $r0
-	xor	$r0, $r0
-	sto.l	-16($fp), $r0
-	xor	$r0, $r0
-	sto.l	-12($fp), $r0
+	ldi.l	$r2, -1
+	ldi.l	$r3, 1
+	xor	$r4, $r4
+	xor	$r1, $r1
 	jmpa	.L2
 .L3:
-	ldo.l	$r1, -8($fp)
-	ldo.l	$r0, -4($fp)
-	mov	$r2, $r1
-	add	$r2, $r0
-	mov	$r0, $r2
-	sto.l	-16($fp), $r0
-	ldo.l	$r0, -8($fp)
-	sto.l	-4($fp), $r0
-	ldo.l	$r0, -16($fp)
-	sto.l	-8($fp), $r0
-	ldo.l	$r0, -12($fp)
-	inc	$r0, 1
-	sto.l	-12($fp), $r0
+	mov	$r4, $r3
+	add	$r4, $r2
+	mov	$r2, $r3
+	mov	$r3, $r4
+	inc	$r1, 1
 .L2:
-	ldo.l	$r1, -12($fp)
-	ldo.l	$r0, 12($fp)
 	cmp	$r1, $r0
 	ble	.L3
-	ldo.l	$r0, -8($fp)
 	ret
 	.global	main
 	.type	main, @function
 main:
-	dec	$sp, 32
+	dec	$sp, 4
 	ldi.l	$r0, 10
-	sto.l	-4($fp), $r0
-	ldo.l	$r0, -4($fp)
 	jsra	fib_iterative
-	sto.l	-8($fp), $r0
-	xor	$r0, $r0
+	sto.l	-4($fp), $r3
 	ret
