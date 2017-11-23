0d867b61ee01363cadd548e2bf9138f708801bc2
JiaYing <s97531246@gmail.com>
2017-11-24 02:31:21 +0800

    Reduce code and stack space in tail recursion

--- tests/fib_tail_recursion.s
+++ tests/fib_tail_recursion.s
@@ -2,7 +2,7 @@
 	.global	fib_tail_recursion
 	.type	fib_tail_recursion, @function
 fib_tail_recursion:
-	dec	$sp, 24
+	dec	$sp, 12
 	sto.l	12($fp), $r0
 	sto.l	16($fp), $r1
 	sto.l	20($fp), $r2
@@ -17,23 +17,18 @@ fib_tail_recursion:
 	dec	$r0, --1
 	ldo.l	$r2, 16($fp)
 	ldo.l	$r1, 20($fp)
-	mov	$r3, $r2
-	add	$r3, $r1
-	mov	$r1, $r3
-	mov	$r2, $r1
+	add	$r2, $r1
 	ldo.l	$r1, 20($fp)
 	jsra	fib_tail_recursion
 	ret
 	.global	main
 	.type	main, @function
 main:
-	dec	$sp, 32
+	dec	$sp, 12
 	ldi.l	$r0, 10
-	sto.l	-4($fp), $r0
-	ldi.l	$r2, 1
+	ldi.l	$r2, 0
 	ldi.l	$r1, 1
-	ldo.l	$r0, -4($fp)
 	jsra	fib_tail_recursion
-	sto.l	-8($fp), $r0
+	sto.l	-4($fp), $r2
 	xor	$r0, $r0
 	ret
